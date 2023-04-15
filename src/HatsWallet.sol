07ba88cd43d24e84b58d97d3be15b4c5c96999a6
spengrah <code@spengrah.xyz>
2023-04-15 18:08:33 +0900

    implementation contract

--- src/HatsWallet.sol
+++ src/HatsWallet.sol
@@ -1,25 +1,39 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
-import "hats-auth/HatsOwned.sol";
+import { Mech } from "mech/base/Mech.sol";
+import { ImmutableStorage } from "mech/base/ImmutableStorage.sol";
+import { IHats } from "hats-protocol/Interfaces/IHats.sol";
 
-contract HatsWallet is HatsOwned {
-    error CallFailed();
-    error CannotChangeHat();
-    constructor(address _hats, uint256 _hatId) HatsOwned(_hatId, _hats) {}
-        
-    function execute(address _target, bytes calldata _data) onlyOwner external payable {
-        (bool success, bytes memory data) = _target.call{value: msg.value}(_data);
+contract HatsWallet is Mech, ImmutableStorage {
+  /// @param _hatsProtocol Address of the Hats Protocol contract
+  /// @param _hatId The token ID of the hat that will control this contract
+  constructor(address _hatsProtocol, uint256 memory _hatId) {
+    bytes memory initParams = abi.encode(_hatsProtocol, _hatId);
+    setUp(initParams);
+  }
+  
+  function setUp(bytes memory initParams) public override {
+    require(readImmutable().length == 0, "Already initialized");
+    // write params as immutables in storage
+    writeImmutable(initParams);
+  }
 
-        if (!success) revert CallFailed();
-    }
+  /// @notice The address of the Hats Protocol contract
+  function hats() public view returns (IHats) {
+    address _hats = abi.decode(readImmutable(), (address));
+    return IHats(_hats);
+  }
 
-    receive() external payable {}
+  /// @notice The hat Id of the hat who's wearer can use this wallet
+  function hat() public view returns (uint256) {
+    (, uint256 _hatId) = abi.decode(readImmutable(), (address, uint256));
+    return _hatId;
+  }
 
-    function setOwnerHat(uint256 _ownerHat, address _hatsContract)
-        public
-        override
-        onlyOwner {
-            revert CannotChangeHat();
-        }
+  /// @notice Checks if `signer` is a valid operator of this wallet, ie if they are a wearer of the {hat()}
+  function isOperator(address signer) public view override returns (bool) {
+    (address _hats, uint256 _hatId,) = abi.decode(readImmutable(), (address, uint256));
+    return hats().isWearerOfHat(signer, hat());
+  }
 }
