d73e0342b7323b282f29379c176888ad66b599dd
spengrah <code@spengrah.xyz>
2023-08-09 20:26:46 -0500

    clean up repo for erc6651 implementation

--- script/HatsWallet.s.sol
+++ script/HatsWallet.s.sol
@@ -3,19 +3,19 @@ pragma solidity ^0.8.18;
 
 import { Script, console2 } from "forge-std/Script.sol";
 import { HatsWallet } from "src/HatsWallet.sol";
-import { HatsWalletFactory } from "src/HatsWalletFactory.sol";
 import { IHats } from "hats-protocol/Interfaces/IHats.sol";
 
-contract DeployFactory is Script {
-  HatsWalletFactory factory;
+contract DeployImplementation is Script {
   HatsWallet implementation;
-  IHats public constant hats = IHats(0x9D2dfd6066d5935267291718E8AA16C8Ab729E9d); // v1.hatsprotocol.eth
+  bool internal verbose;
+  IHats public constant HATS = IHats(0x3bc1A0Ad72417f2d411118085256fC53CBdDd137); // v1.hatsprotocol.eth
   //   string public version = "0.1.0"; // increment with each deploy
   bytes32 internal constant SALT = bytes32(abi.encode(0x4a75)); // ~ H(4) A(a) T(7) S(5)
 
-  //   function prepare(string memory _version) public {
-  //     version = _version;
-  //   }
+  function prepare(bool _verbose, string memory _version) public {
+    verbose = _verbose;
+    version = _version;
+  }
 
   function run() public {
     uint256 privKey = vm.envUint("PRIVATE_KEY");
@@ -24,12 +24,10 @@ contract DeployFactory is Script {
     vm.startBroadcast(deployer);
     // deploy the implementation
     implementation = new HatsWallet{ salt: SALT }(address(0), 0);
-    // deploy the contract
-    factory = new HatsWalletFactory{ salt: SALT }(implementation, hats);
-    vm.stopBroadcast();
 
-    console2.log("implementation", address(implementation));
-    console2.log("factory", address(factory));
+    if (verbose) {
+      console2.log("implementation", address(implementation));
+    }
   }
   // forge script script/HatsWallet.s.sol:DeployFactory -f mainnet --broadcast --verify
 }
--- src/HatsWallet.sol
+++ src/HatsWallet.sol
@@ -1,39 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
-import { Mech } from "foundry-mech/base/Mech.sol";
-import { ImmutableStorage } from "foundry-mech/base/ImmutableStorage.sol";
+// import { console2 } from "forge-std/Test.sol"; // remove before deploy
 import { IHats } from "hats-protocol/Interfaces/IHats.sol";
+import { IERC6551Account } from "erc6551/interfaces/IERC6551Account.sol";
+import { IERC6551Executable } from "erc6551/interfaces/IERC6551Executable.sol";
 
-contract HatsWallet is Mech, ImmutableStorage {
-  /// @param _hatsProtocol Address of the Hats Protocol contract
-  /// @param _hatId The token ID of the hat that will control this contract
-  constructor(address _hatsProtocol, uint256 _hatId) {
-    bytes memory initParams = abi.encode(_hatsProtocol, _hatId);
-    setUp(initParams);
-  }
-
-  function setUp(bytes memory initParams) public override {
-    require(readImmutable().length == 0, "Already initialized");
-    // write params as immutables in storage
-    writeImmutable(initParams);
-  }
-
-  /// @notice The address of the Hats Protocol contract
-  function HATS() public view returns (IHats) {
-    address _hats = abi.decode(readImmutable(), (address));
-    return IHats(_hats);
-  }
-
-  /// @notice The hat Id of the hat who's wearer can use this wallet
-  function hat() public view returns (uint256) {
-    (, uint256 _hatId) = abi.decode(readImmutable(), (address, uint256));
-    return _hatId;
-  }
-
-  /// @notice Checks if `signer` is a valid operator of this wallet, ie if they are a wearer of the {hat()}
-  function isOperator(address signer) public view override returns (bool) {
-    (address _hats, uint256 _hatId) = abi.decode(readImmutable(), (address, uint256));
-    return IHats(_hats).isWearerOfHat(signer, _hatId);
-  }
-}
+contract HatsWallet is IERC6551Account, IERC6551Executable { }
